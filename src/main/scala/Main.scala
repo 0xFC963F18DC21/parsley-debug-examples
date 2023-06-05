5d80b9735f7e74f6692155477257034c59861d82
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-06-05 15:19:23 +0100

    Example: numbers.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
+import examples._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    // Run all examples.
+    // Feel free to comment examples out to only run a select few.
+    val examples: List[Runnable] = List(
+      Numbers
+    )
+
+    for (example <- examples) {
+      example.run()
+    }
   }
 }
---
+++ src/main/scala/examples/Numbers.scala
@@ -0,0 +1,40 @@
+package examples
+
+import parsley.Parsley
+import parsley.character._
+import parsley.expr.chain
+
+object Numbers extends Runnable {
+  // A subtle issue with this parser is how some numbers just do not parse as they should.
+  // It may take you either a few seconds or a few minutes to figure it out by looking at it,
+  // especially if this is used in a much larger parser.
+  // So, what is the problem with this parser?
+  // Note that in your fix, the parser doesn't have to have the same types, per-se.
+  // We just want a working (mathematical) integer parser.
+  val brokenIntegerParser: Parsley[Int] =
+    chain.prefix(
+      char('-') #> ((_: Int) * -1),
+      satisfy(_.isDigit).foldLeft1(0)((t, c) => c.asDigit + (t * 10))
+    )
+
+  // Implement your fixed parser here. Feel free to change the type.
+  val fixedIntegerParser: Parsley[Any] = Parsley.empty
+
+  override def run(): Unit = {
+    // Input cases.
+    val cases: List[String] = List(
+      "123",
+      "-123",
+      "12345689",
+      "-123456789",
+      "9876543210",
+      "-9876543210"
+    )
+
+    // Running the parser itself.
+    for (inp <- cases) {
+      println(s"BROKEN: $inp -> ${brokenIntegerParser.parse(inp)}")
+      println(s" FIXED: $inp -> ${fixedIntegerParser.parse(inp)}")
+    }
+  }
+}
